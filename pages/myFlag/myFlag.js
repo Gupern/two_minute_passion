d6fe14245b74e2efd3d9a74fb5e8c763eb82c35b
Gupern <guperner@hotmail.com>
2022-03-15 22:16:23 +0800

    feat: 完成 “我的flag”页面

---
+++ pages/myFlag/myFlag.js
@@ -0,0 +1,111 @@
+// pages/myFlag/myFlag.js
+const app = getApp()
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        project: [{
+            "projectName": "project name",
+            "taskList": [{
+                "name": "task name",
+                "count": 3
+            }, {
+                "name": "夸人",
+                "count": 4
+            }]
+
+        }, {
+
+            "projectName": "project name",
+            "taskList": [{
+                "name": "task name",
+                "count": 3
+            }, {
+                "name": "夸人",
+                "count": 4
+            }]
+
+        }]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 请求后台接口，获取数据
+        this.getPersonalProjectInfo();
+    },
+
+    /**
+     * 获取个人的项目数据
+     */
+    getPersonalProjectInfo() {
+        console.log("openid:", app.globalData.openid)
+        wx.request({
+            url: app.globalData.defaultURL + "/wechat/miniprogram/get_personal_project_info",
+            method: "POST",
+            header: {
+                "content-type": "application/json"
+            },
+            data: {
+                openid: app.globalData.openid
+            },
+            success: (res) => {
+                console.log("get_personal_project_info res", res);
+                this.setData({
+                    project: res.data.data
+                })
+            }
+        })
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
